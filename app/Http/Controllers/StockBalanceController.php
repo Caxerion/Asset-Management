0840b0943fa264c4b26365f5be91d1b782ce1dd8
Caxerion <kennethgeveny1@gmail.com>
2026-02-27 08:40:08 +0700

    Refactor all controllers for consistency and improved workflow

---
+++ app/Http/Controllers/StockBalanceController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StockBalance;
+use Illuminate\Http\Request;
+
+class StockBalanceController extends Controller
+{
+    public function index()
+    {
+        return StockBalance::with('product','floor')->get();
+    }
+
+    public function store(Request $request)
+    {
+        $stock = StockBalance::create($request->validate([
+            'product_id' => 'required|exists:products,id',
+            'floor_id' => 'required|exists:floors,id',
+            'qty_on_hand' => 'required|numeric'
+        ]));
+        return response()->json($stock, 201);
+    }
+
+    public function show(StockBalance $stockBalance)
+    {
+        return $stockBalance->load('product','floor');
+    }
+
+    public function update(Request $request, StockBalance $stockBalance)
+    {
+        $stockBalance->update($request->validate([
+            'qty_on_hand' => 'required|numeric'
+        ]));
+        return $stockBalance;
+    }
+
+    public function destroy(StockBalance $stockBalance)
+    {
+        $stockBalance->delete();
+        return response()->noContent();
+    }
+}
